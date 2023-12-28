08ce6f07f98aa0ecd20b43ce099c2c608fb507c0
Niklas Max G <132490838+AbUndMax@users.noreply.github.com>
2023-12-28 23:35:24 +0100

    Update cat-hunt-food-game.rkt
    added space between Header-line and imports

--- cat-hunt-food-game.rkt
+++ cat-hunt-food-game.rkt
@@ -1,6 +1,7 @@
 ;; The first three lines of this file were inserted by DrRacket. They record metadata
 ;; about the language level of this file in a form that our tools can easily process.
 #reader(lib "htdp-beginner-reader.ss" "lang")((modname cat-hunt-food-game) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+
 (require 2htdp/image)
 (require 2htdp/universe)
 
